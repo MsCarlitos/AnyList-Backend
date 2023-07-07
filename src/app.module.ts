9ad7f6c37a3261a4ed70dd3dc9a570bfe747e38e
MsCarlitos <camendozacontreras5@gmail.com>
2023-07-07 13:31:07 -0700

    Autenticación y autorización

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { join } from 'path';
-import { Module } from '@nestjs/common';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloDriverConfig, ApolloDriver } from '@nestjs/apollo';
-import { ApolloServerPluginLandingPageLocalDefault } from '@apollo/server/plugin/landingPage/default';
 
-import { ItemModule } from './item/item.module';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+
+import { ItemsModule } from './items/items.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -15,20 +18,25 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       driver: ApolloDriver,
       playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
-      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      plugins: [
+        ApolloServerPluginLandingPageLocalDefault
+      ]
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST,
+      host: 'localhost',
       port: +process.env.DB_PORT,
       username: process.env.DB_USERNAME,
-      password: process.env.BD_PASSWORD,
+      password: process.env.DB_PASSWORD,
       database: process.env.DB_NAME,
       synchronize: true,
       autoLoadEntities: true,
     }),
-
-    ItemModule,
+    ItemsModule,
+    UsersModule,
+    AuthModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
